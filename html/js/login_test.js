6f99baf2aab5fe497be719664bfd60b4dc7ae4cc
Samzou <samzou1997@gmail.com>
2020-09-02 00:52:40 +1000

    Update login_test.js

--- html/js/login_test.js
+++ html/js/login_test.js
@@ -5,8 +5,8 @@ var postHTML =
     '<html><head><meta charset="utf-8"><title>菜鸟教程 Node.js 实例</title></head>' +
     '<body>' +
     '<form method="post">' +
-    '网站名： <input name="name"><br>' +
-    '网站 URL： <input name="url"><br>' +
+    '网站名： <input name="uid"><br>' +
+    '网站 URL： <input name="pwd"><br>' +
     '<input type="submit">' +
     '</form>' +
     '</body></html>';
