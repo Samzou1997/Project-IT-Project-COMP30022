4e876ec1e73755ad27acc95bd4dd7e8d92f18321
Samzou <samzou1997@gmail.com>
2020-09-02 01:20:49 +1000

    login test

--- html/js/login_test.js
+++ html/js/login_test.js
@@ -42,3 +42,33 @@ http.createServer(function (req, res) {
 }).listen(6767, function () {
     console.log("Server on")
 });
+
+server.on('request', function (request, response) {
+    console.log('got request, path: ' + request.url)
+    response.setHeader('Content-Type', 'text/plain; charset=utf-8')
+    response.end('Login Test')
+    // fs.readFile("."+request.url, 'utf-8', function (err, data) {
+    //   var pathname = url.parse(request.url).pathname
+    //   var ext = pathname.match(/(\.[^.]+|)$/)[0]
+    //   if (err) {
+    //     response.setHeader('Content-Type', 'text/plain; charset=utf-8')
+    //     response.end('File error')
+    //   }
+    //   else {
+    //     switch(ext) {
+    //       case ".html":
+    //         response.setHeader('Content-Type', 'text/html; charset=utf-8')
+    //         break
+    //       case ".css":
+    //         response.setHeader('Content-Type', 'text/css; charset=utf-8')
+    //         break
+    //       case ".jpg":
+    //         response.setHeader('Content-Type', 'image/jpeg')
+    //         break
+    //       default:
+    //         response.setHeader('Content-Type', 'image/jpeg')
+    //     }
+    //     response.end(data)
+    //   }
+    // })
+  })
