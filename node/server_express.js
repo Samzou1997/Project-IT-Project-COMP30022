aa014a1592cda7e44425bc91e27ab6d60836f899
Samzou <samzou1997@gmail.com>
2020-09-03 21:45:51 +1000

    Update server_express.js

--- node/server_express.js
+++ node/server_express.js
@@ -12,25 +12,10 @@ app.use(bodyParser.urlencoded({
 /* 接受请求并反馈数据渲染到界面*/
 app.post("/Login",function(req,res){
     console.log('got request, path: ' + req.url)
-    req.rawBody = '';//添加接收变量
-    var body;
-    req.setEncoding('utf8');
-
-    req.on('data', function(chunk) { 
-        req.rawBody += chunk;
-    });
-
-    req.on('end', function() {
-        body = querystring.parse(rawBody);
-        console.log("body:",body);
-        res.send(body.uid.toString());
-    }); 
+    console.log('got request, body: ' + req.body)
+    res.send(req.body);
 })
 
 app.listen(3000,function(){
     console.log("running....");
-})
-
-app.on('request', function (request, response) {
-    console.log('got request, path: ' + request.url)
 })
