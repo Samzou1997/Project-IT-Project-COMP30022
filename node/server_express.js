f1db723030335a7e0e1445b14203cadcb169799d
Samzou <samzou1997@gmail.com>
2020-09-03 20:51:02 +1000

    Update server_express.js

--- node/server_express.js
+++ node/server_express.js
@@ -1,11 +1,12 @@
 var express =require("express");
 var bodyParser = require('body-parser');
 
+var app = express();
+
 app.use(bodyParser.urlencoded({
     extended:true
 }));
 /* 创建服务器 */
-var app = express();
 
 /* 接受请求并反馈数据渲染到界面*/
 app.get("/",function(req,res){
