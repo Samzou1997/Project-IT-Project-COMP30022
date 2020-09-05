0017bfa26103c2d0b542c164a1b4795de52eea91
Samzou <samzou1997@gmail.com>
2020-09-06 00:53:59 +1000

    Update TokenVerifier.js

--- html/controllers/TokenVerifier.js
+++ html/controllers/TokenVerifier.js
@@ -1,24 +1,29 @@
 const jwt = require('jsonwebtoken')
+const { resolve } = require('path')
+const { rejects } = require('assert')
 
 
 exports.verifyToken = function(token, key, email, id){
-  jwt.verify(token, key, function(error, decoded){
-    if (error) {
-      console.log("token decode error or invaild")
-      return false
-    }
-    
-    else {
-      console.log('raw: ' + email + ' ' + id)
-      console.log('decode: ' + decoded.user_email + ' ' + decoded.user_id)
-      if ((decoded.user_email === email) && (decoded.user_id === id)){
-        console.log('token vaild')
-        return true
+  return new Promise((resolve, rejects) => {
+    jwt.verify(token, key, function(error, decoded){
+      if (error) {
+        console.log("token decode error or invaild")
+        resolve(false) 
       }
+      
       else {
-        console.log('token invaild')
-        return false
-      }
-    }  
+        console.log('raw: ' + email + ' ' + id)
+        console.log('decode: ' + decoded.user_email + ' ' + decoded.user_id)
+        if ((decoded.user_email === email) && (decoded.user_id === id)){
+          console.log('token vaild')
+          resolve(true) 
+        }
+        else {
+          console.log('token invaild')
+          resolve(false) 
+        }
+      }  
+    })
   })
+  
 }
