7180f80eff139a98ef845a9185e9e9e2e3b12949
Samzou <samzou1997@gmail.com>
2020-09-06 00:34:20 +1000

    login

--- html/controllers/TokenVerifier.js
+++ html/controllers/TokenVerifier.js
@@ -4,16 +4,19 @@ const jwt = require('jsonwebtoken')
 exports.verifyToken = function(token, key, email, id){
   jwt.verify(token, key, function(error, decoded){
     if (error) {
-      console.log("token decode error")
+      console.log("token decode error or invaild")
       return false
     }
     
     else {
+      console.log('raw: ' + email + ' ' + id)
       console.log('decode: ' + decoded.user_email + ' ' + decoded.user_id)
       if ((decoded.user_email === email) && (decoded.user_id === id)){
+        console.log('token vaild')
         return true
       }
       else {
+        console.log('token invaild')
         return false
       }
     }  
