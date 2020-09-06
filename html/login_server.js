187789ef52ca283a61dfe1f73bbc99c0eec31dc3
Samzou <samzou1997@gmail.com>
2020-09-06 23:32:20 +1000

    Update login_server.js

--- html/login_server.js
+++ html/login_server.js
@@ -39,98 +39,136 @@ app.use(cookieParser())
 app.post("/Login",function(req,res){
     console.log('got Login request, path: ' + req.url)
     console.log('request body: { email: ' + req.body.email + ", pwd: " + req.body.password + " }")
-    
-    // if there are cookies included in the request
-    if (req.cookies["email"] != null){
-        let req_token = req.cookies['token']
-        let req_user_id = req.cookies['id']
-        let req_user_email = req.cookies['email']
 
-        //verify token
-        jwt.verify(req_token, secret_key, function(error, decoded){
-            if (error) {
-                User.findOne({email: req.body.email}, function(err, doc){
-                    let user_password = doc.password
-                    if (err) {
-                        console.log("db error")
-                    }
-                    if (doc) {
-                        if (user_password === req.body.password){
-                            let user_email = doc.email
-                            let user_id = doc._id
-                            let token = jwt.sign({user_id, user_email}, secret_key, {expiresIn: 60})
-        
-                            res.cookie('id', user_id, { maxAge: alive_time })
-                            res.cookie('email', user_email, { maxAge: alive_time })
-                            res.cookie('token', token, { maxAge: alive_time })
-                            res.redirect('http://3.131.49.106/home')
-                            // res.render('home.html', {
-                            //     username: doc.lastName
-                            // })
-                        }
-                        else{
-                            res.render("index.html", {
-                                login_error_message: "Incorrect password.",
-                                register_error_message: ""
-                            })
-                        }
-                    }
-                    else {
-                        res.render("index.html", {
-                            login_error_message: "No user found.",
-                            register_error_message: ""
-                        })
-                    }
-                })
-            }
-            else {
+    res.cookie('id', '', { maxAge: 0 })
+    res.cookie('email', '', { maxAge: 0 })
+    res.cookie('token', '', { maxAge: 0 })
+
+    User.findOne({email: req.body.email}, function(err, doc){
+        let user_password = doc.password
+        if (err) {
+            console.log("db error")
+        }
+        if (doc) {
+            if (user_password === req.body.password){
+                let user_email = doc.email
+                let user_id = doc._id
+                let token = jwt.sign({user_id, user_email}, secret_key, {expiresIn: 60})
+
+                res.cookie('id', user_id, { maxAge: alive_time })
+                res.cookie('email', user_email, { maxAge: alive_time })
+                res.cookie('token', token, { maxAge: alive_time })
                 res.redirect('http://3.131.49.106/home')
-                // User.findOne({email: decoded.user_email}, function(err, doc){
-                //     if (err) {
-                //         console.log("db error")
-                //     }
-                //     res.render('home.html', {
-                //         username: doc.lastName
-                //     })
+                // res.render('home.html', {
+                //     username: doc.lastName
                 // })
-            }  
-        })
-    }
-    else {
-        User.findOne({email: req.body.email}, function(err, doc){
-            let user_password = doc.password
-            if (err) {
-                console.log("db error")
             }
-            if (doc) {
-                if (user_password === req.body.password){
-                    let user_email = doc.email
-                    let user_id = doc._id
-                    let token = jwt.sign({user_id, user_email}, secret_key, {expiresIn: 60})
-
-                    res.cookie('id', user_id, { maxAge: alive_time })
-                    res.cookie('email', user_email, { maxAge: alive_time })
-                    res.cookie('token', token, { maxAge: alive_time })
-                    res.redirect('http://3.131.49.106/home')
-                    // res.render('home.html', {
-                    //     username: doc.lastName
-                    // })
-                }
-                else{
-                    res.render("index.html", {
-                        login_error_message: "Incorrect password.",
-                        register_error_message: ""
-                    })
-                }
-            }
-            else {
+            else{
                 res.render("index.html", {
-                    login_error_message: "No user found.",
+                    login_error_message: "Incorrect password.",
                     register_error_message: ""
                 })
             }
-        })
-    }
+        }
+        else {
+            res.render("index.html", {
+                login_error_message: "No user found.",
+                register_error_message: ""
+            })
+        }
+    })
+
+    // if there are cookies included in the request
+    // if (req.cookies["email"] != null){
+    //     let req_token = req.cookies['token']
+    //     let req_user_id = req.cookies['id']
+    //     let req_user_email = req.cookies['email']
+
+    //     //verify token
+    //     jwt.verify(req_token, secret_key, function(error, decoded){
+    //         if (error) {
+    //             User.findOne({email: req.body.email}, function(err, doc){
+    //                 let user_password = doc.password
+    //                 if (err) {
+    //                     console.log("db error")
+    //                 }
+    //                 if (doc) {
+    //                     if (user_password === req.body.password){
+    //                         let user_email = doc.email
+    //                         let user_id = doc._id
+    //                         let token = jwt.sign({user_id, user_email}, secret_key, {expiresIn: 60})
+        
+    //                         res.cookie('id', user_id, { maxAge: alive_time })
+    //                         res.cookie('email', user_email, { maxAge: alive_time })
+    //                         res.cookie('token', token, { maxAge: alive_time })
+    //                         res.redirect('http://3.131.49.106/home')
+    //                         // res.render('home.html', {
+    //                         //     username: doc.lastName
+    //                         // })
+    //                     }
+    //                     else{
+    //                         res.render("index.html", {
+    //                             login_error_message: "Incorrect password.",
+    //                             register_error_message: ""
+    //                         })
+    //                     }
+    //                 }
+    //                 else {
+    //                     res.render("index.html", {
+    //                         login_error_message: "No user found.",
+    //                         register_error_message: ""
+    //                     })
+    //                 }
+    //             })
+    //         }
+    //         else {
+    //             res.redirect('http://3.131.49.106/home')
+    //             // User.findOne({email: decoded.user_email}, function(err, doc){
+    //             //     if (err) {
+    //             //         console.log("db error")
+    //             //     }
+    //             //     res.render('home.html', {
+    //             //         username: doc.lastName
+    //             //     })
+    //             // })
+    //         }  
+    //     })
+    // }
+    // else {
+    //     User.findOne({email: req.body.email}, function(err, doc){
+    //         let user_password = doc.password
+    //         if (err) {
+    //             console.log("db error")
+    //         }
+    //         if (doc) {
+    //             if (user_password === req.body.password){
+    //                 let user_email = doc.email
+    //                 let user_id = doc._id
+    //                 let token = jwt.sign({user_id, user_email}, secret_key, {expiresIn: 60})
+
+    //                 res.cookie('id', user_id, { maxAge: alive_time })
+    //                 res.cookie('email', user_email, { maxAge: alive_time })
+    //                 res.cookie('token', token, { maxAge: alive_time })
+    //                 res.redirect('http://3.131.49.106/home')
+    //                 // res.render('home.html', {
+    //                 //     username: doc.lastName
+    //                 // })
+    //             }
+    //             else{
+    //                 res.render("index.html", {
+    //                     login_error_message: "Incorrect password.",
+    //                     register_error_message: ""
+    //                 })
+    //             }
+    //         }
+    //         else {
+    //             res.render("index.html", {
+    //                 login_error_message: "No user found.",
+    //                 register_error_message: ""
+    //             })
+    //         }
+    //     })
+    // }
 })
 
 app.get("/Login", function(req, res){
