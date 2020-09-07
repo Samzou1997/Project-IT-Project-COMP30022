d020301d47e1d8e1e5d0a48c88e5e76e203aa622
Chang Liu <liuc18@unimelb.edu.au>
2020-09-07 20:32:57 +0800

    Update unit_test.py

--- Testing/unit_test.py
+++ Testing/unit_test.py
@@ -3,11 +3,14 @@ import time
 
 driver = webdriver.Chrome('/Users/zehongli/Documents/GitHub/Project-IT-Project-COMP30022/Testing/chromedriver')
 driver.fullscreen_window()
+driver.implicitly_wait(10)
 driver.get('http://3.131.49.106/')
 
 # Testing valid email && valid password
 driver.find_element_by_id("uid-input-login").send_keys('873651425@qq.com')
 driver.find_element_by_id("pwd-input-login").send_keys('qpzm123456\n')
+
+driver.find_element_by_class_name("btn solid").click()
 
 
 time.sleep(5)
