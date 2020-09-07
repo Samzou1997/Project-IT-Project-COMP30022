e6664789e97569e8b1443235fea5e1c6fc537095
Samzou <samzou1997@gmail.com>
2020-09-08 03:43:02 +1000

    Update RegisterRouter.js

--- html/routes/RegisterRouter.js
+++ html/routes/RegisterRouter.js
@@ -3,7 +3,6 @@ const router    = express.Router()
 
 const RegisterController   = require('../controllers/RegisterController')
 
-
 router.post('/', RegisterController.register_post)
 router.get('/', RegisterController.register_get)
 
