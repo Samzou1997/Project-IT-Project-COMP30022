a5b69cb9aef21650218c651212f066e8bc7f3305
Peter-Kai <69135733+Peter-Kai@users.noreply.github.com>
2020-09-22 21:19:19 +1000

    Update testemailsend.js

--- html/testemailsend.js
+++ html/testemailsend.js
@@ -3,4 +3,4 @@ var email = "yuxuekuangmo@gmail.com";
 var subject = "test";
 var text =undefined;
 var html = "<p>test</p><p>To reset password</p><p>click the link below：</p><p><a href='https://cn.pornhub.com/front/lost_password'>reset your password</a></p>";;
-sender.emailTo(email, subject, text, html)
+sender.emailTo(email, subject, text, html);
