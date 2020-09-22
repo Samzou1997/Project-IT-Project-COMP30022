9b16d80205658e1e4024bf2c9f9f0a97cc1300a7
PerterKai <69103247+PerterKai@users.noreply.github.com>
2020-09-22 22:57:48 +1000

    Delete testemailsend.js

--- html/testemailsend.js
+++
@@ -1,6 +0,0 @@
-var sender =  require('./send_email.js')
-var email = "yuxuekuangmo@gmail.com";
-var subject = "test";
-var text =undefined;
-var html = "<p>test</p><p>To reset password</p><p>click the link below：</p><p><a href='https://cn.pornhub.com/front/lost_password'>reset your password</a></p>";;
-sender.emailTo(email, subject, text, html);
