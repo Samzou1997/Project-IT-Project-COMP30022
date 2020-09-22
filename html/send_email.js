83466b7121ae18fd0dcaf17115214fc58844a30e
PerterKai <69103247+PerterKai@users.noreply.github.com>
2020-09-22 19:44:52 +1000

    email sending cunction

---
+++ html/send_email.js
@@ -0,0 +1,52 @@
+//import nodemailer for setup smtp and send email
+var nodemailer = require("nodemailer");
+
+//the smtp service address and acount use for sending link
+var smtp = "smtp.gmail.com"
+var mailFrom = "team5eportifolio@gmail.com"
+var mailPwd = "Abc12345.."
+
+function emailTo(email,subject,text,html,callback) {
+    var transporter = nodemailer.createTransport({
+        host: smtp,
+        auth: {
+            user: mailFrom,
+            pass: mailPwd 
+
+        }
+    });
+    var mailOptions = {
+        from: mailFrom,
+        to: email,
+        subject: subject,
+    };
+    if(text != undefined)
+    {
+        mailOptions.text =text;
+    }
+    if(html != undefined)
+    {
+        mailOptions.html =html;
+    }
+
+    var result = {
+        httpCode: 200,
+        message: 'success',
+    }
+    try {
+        transporter.sendMail(mailOptions, function (err, info) {
+            if (err) {
+                result.httpCode = 500;
+                result.message = err;
+                callback(result);
+                return;
+            }
+            callback(result);
+        });
+    } catch (err) {
+        result.httpCode = 500;
+        result.message = err;
+        callback(result);
+    }
+
+}
