43951140d99dcdf1fd83a699b25d2fb88e8c1ac8
Samzou <samzou1997@gmail.com>
2020-09-22 23:31:15 +1000

    Update User.js

--- html/models/User.js
+++ html/models/User.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require('bson')
 const mongoose    = require('mongoose')
 const Schema      = mongoose.Schema
 
@@ -20,21 +21,33 @@ const userSchema    = new Schema({
   },
   detail: {
     type: Schema.Types.Mixed,
-    default: {
-      gender: {
-        type: String,
-        default: 'null'
-      },
-      dateBirth: {
-        type: String,
-        default: 'null'
-      }
-    }
+    default: {}
   }
-  //avatar:{
-  //  type: String,
-  //  default: '/public/..'
 },{timestamps: true})
+
+// users collection example:
+var example = {
+  "_id" : ObjectId("5f69ce5f5f8cd7751b793c81"),
+  
+  "firstName" : "xxx",
+  "lastName" : "xxx",
+  "email" : "xxx@xxx.com",
+  "password" : "e10adc3949ba59abbe56e057f20f883e",
+  "detail" : {
+    "major" : "IT",
+    "school" : "xxx",
+    "degree" : "xxx",
+    "gender" : "",
+    "dateBirth" : "",
+    "phone" : "0444xxxxxx",
+    "address" : "xxxx xxxx xxx",
+  },
+  "setting" : {
+    "$ref": "userSetting",
+    "$id": ObjectId("534009e4d852427820000002"),
+    "$db": "Geek",
+  }
+}
 
 const User = mongoose.model('User', userSchema)
 module.exports = User
