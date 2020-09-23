0999449c20fb9ad6ff586c993ecf6b8d6fabcbf3
Samzou <samzou1997@gmail.com>
2020-09-23 20:51:25 +1000

    Update RegisterController.js

--- html/controllers/RegisterController.js
+++ html/controllers/RegisterController.js
@@ -44,15 +44,13 @@ const register_post = (req, res, next) => {
           var fiber = Fiber.current
           userSetting.save().then(userSetting => {
             user.setting.$id = userSetting._id
-            fiber.run()
           }).catch(error => {
             console.log(error)
             res.render("register_error.html", {
               message: "system error, try again :)"
             })
-            fiber.run()
           })
-
+          fiber.run()
         })
         Fiber.yield()
 
