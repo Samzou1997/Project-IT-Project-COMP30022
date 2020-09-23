6d2f674af6af761988d78fd16eb86705a1aac45a
PerterKai <69103247+PerterKai@users.noreply.github.com>
2020-09-23 17:14:03 +1000

    update

--- html/routes/ResetRouter.js
+++ html/routes/ResetRouter.js
@@ -4,6 +4,6 @@ const router    = express.Router()
 const ResetController   = require('../controllers/ResetController')
 
 router.get('/', ResetController.resetpage)
-router.post('/reset', ResetController.emailTo)
+router.post('/Reset', ResetController.emailTo)
 
 module.exports = router
