46d69565ae12a7346c8c0c7798267f8cde5711ff
Peter-Kai <69135733+Peter-Kai@users.noreply.github.com>
2020-09-25 01:52:48 +1000

    Update ResetController.js

--- html/controllers/ResetController.js
+++ html/controllers/ResetController.js
@@ -27,7 +27,13 @@ const emailTo = (req, res, next) => {
             let updatedData = {
                 passwordRestToken: token
             }
-            UserData.findByIdAndUpdate(userid, {$set: updatedData});
+            UserData.findByIdAndUpdate(userid, {$set: updatedData})
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            })
             var subject = "Reset your password for your account"
             var text = undefined;
             var html = "<p>test</p><p>To reset password</p><p>click the link below：</p><p><a href='http://54.206.15.44/Forgot/Resetting/"+token+">reset your password</a></p>";
