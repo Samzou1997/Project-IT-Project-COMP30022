e97ed18bd696d1a548c58cd98b80fe03f018f93a
Chang Liu <liuc18@unimelb.edu.au>
2020-09-30 21:59:43 +0800

    add test case

--- Testing/APItest/Base/ResponseVerify.py
+++ Testing/APItest/Base/ResponseVerify.py
@@ -1,7 +1,6 @@
 def responseVerify(res, expected):
-    print(res.status_code)
-    print(expected["status_code"])
     if (str(res.status_code) == expected["status_code"]):
         return "succeed"
     else:
+        print("Error:", res.status_code)
         return "failed"
