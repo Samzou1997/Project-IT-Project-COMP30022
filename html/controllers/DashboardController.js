ea6b2baf9ec636b5841efe9265cdf41027e8858b
Samzou <samzou1997@gmail.com>
2020-09-30 14:13:23 +1000

    dubug

--- html/controllers/DashboardController.js
+++ html/controllers/DashboardController.js
@@ -4,18 +4,11 @@ var cookieParser      = require('cookie-parser')
 const jwt             = require('jsonwebtoken')
 const config          = require('../config/web_config.json')
 var dashboardPaddingData      = require('../views/data_padding/dashboard_data.json')
-var profileEditPaddingData    = require('../views/data_padding/profile_edit.json')
 const FileSystemController    = require('../controllers/FileSystemController')
 const path                    = require('path');
 const fs                      = require('fs');
 
-const secret_key          = config.token_setting.secret_key
-const token_expire_time   = config.token_setting.expire_time
-const cookie_alive_time   = config.cookie_setting.alive_time
-const userDataDir         = "/home/IT_Project/html/file/userData/";
-
 var userID_str;
-
 
 const dashboard_get = (req, res, next) => {
   User.findOne({ email: req.cookies["email"] }, function (err, doc) {
@@ -33,6 +26,7 @@ const dashboard_get = (req, res, next) => {
       var documentDir = path.join(config.fileSystem.userDataDir, userID_str, config.fileSystem.dashboard_document);
 
       dashboardPaddingData.content = documentDir
+      dashboardPaddingData.lastname = doc.lastName
       
 
       res.render('dashboard.html', dashboardPaddingData)
