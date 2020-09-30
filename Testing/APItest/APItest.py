c57c109ffcfec93f62554f44fa82a8b57b8d7294
Chang Liu <liuc18@unimelb.edu.au>
2020-09-30 19:07:17 +0800

    APItest

--- Testing/APItest/APItest.py
+++ Testing/APItest/APItest.py
@@ -1,35 +1,33 @@
-import requests
 import json
 import os
-
-def test_get(url, data, headers):   
-    data = json.dumps(data)
-
-    #send request
-    res = requests.get(url = url,params = data, headers = headers)
-    print("--------------------")
-    print("response status code:",res.status_code)
-    print("response headers",res.headers)
-
-def test_post(url, data, headers):
-    data = json.dumps(data)
-
-    #send request
-    res = requests.post(url = url, data = data, headers = headers)
-
-    print("--------------------")
-    print("response status code:",res.status_code)
-    print("response headers",res.headers)
+import traceback
+from TestCase.test import *
         
-#test()
+print("API test start...")
+
+#find path for test case data
 path = os.getcwd()
-path = os.path.join(path,"data.json")
-with open(path,'r') as load_f:
-    load_dict = json.load(load_f)
-    #print(load_dict[0])
-    #print(type(load_dict))
-    for testcase in load_dict:
-        if (testcase["request_method"] == "post"):
-            test_post(testcase["url"], testcase["data"], testcase["headers"])
-        if (testcase["request_method"] == "get"):
-            test_get(testcase["url"], testcase["data"], testcase["headers"])
+path = os.path.join(path,"TestData/data.json")
+
+#case counting
+total_case = 0
+succeed_case = 0
+failed_case = 0
+
+try:
+    with open(path,'r',encoding = 'utf-8') as load_f:
+        load_dict = json.load(load_f)
+        for testcase in load_dict:
+            total_case += 1
+            result = test_case(testcase)
+            if result:
+                succeed_case += 1
+            else:
+                failed_case += 1
+except Exception:
+    traceback.print_exc()
+
+print("--------------------")
+print("Total test case:", total_case)
+print("Succeed test case:", succeed_case)
+print("Failed test case:", failed_case)
---
+++ Testing/APItest/TestCase/test.py
@@ -0,0 +1,35 @@
+import requests
+import json
+
+def test_case(testcase):
+
+    print("--------------------")
+    print(testcase["test_name"], "start...")
+
+    result = "succeed."
+    
+
+    #read data, find request
+    test_req = testcase["request"]
+    method = test_req["request_method"]
+    url = test_req["url"]   
+    data = json.dumps(test_req["data"])
+    headers = test_req["headers"]
+
+    #send request
+    if (method == "post"):
+        res = requests.post(url = url, data = data, headers = headers)
+    elif (method == "get"):
+        res = requests.get(url = url,params = data, headers = headers)
+    else:
+        print("Error:", testcase["test_name"], "request_method error.")
+        result = "failed."
+        return False
+    
+    #res_json = res.json()
+    #json.dumps(res_json)
+
+    #print("response status code:",res.status_code)
+    print("response headers",res)
+    print(testcase["test_name"], result)
+    return True
