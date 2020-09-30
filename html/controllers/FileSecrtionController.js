579a5c8f02654ef5ff60be2210ca93472c16c7e4
Samzou <samzou1997@gmail.com>
2020-09-30 17:35:33 +1000

    debug

--- html/controllers/FileSecrtionController.js
+++ html/controllers/FileSecrtionController.js
@@ -1,16 +1,16 @@
-const User            = require('../models/User')
-const { response }    = require('express')
-var cookieParser      = require('cookie-parser')
-const jwt             = require('jsonwebtoken')
-const config          = require('../config/web_config.json')
-const path            = require('path');
-const FileSystemController    = require('../controllers/FileSystemController')
-var ManageFileData            = require('../views/data_padding/manage_file_data.json')
+const User = require('../models/User')
+const { response } = require('express')
+var cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken')
+const config = require('../config/web_config.json')
+const path = require('path');
+const FileSystemController = require('../controllers/FileSystemController')
+var ManageFileData = require('../views/data_padding/manage_file_data.json')
 
-const secret_key          = config.token_setting.secret_key
-const token_expire_time   = config.token_setting.expire_time
-const cookie_alive_time   = config.cookie_setting.alive_time
-const userDataDir         = "/home/IT_Project/html/file/userData/";
+const secret_key = config.token_setting.secret_key
+const token_expire_time = config.token_setting.expire_time
+const cookie_alive_time = config.cookie_setting.alive_time
+const userDataDir = "/home/IT_Project/html/file/userData/";
 
 const file_section_get = (req, res, next) => {
   User.findOne({ email: req.cookies["email"] }, function (err, doc) {
@@ -47,18 +47,27 @@ const file_upload_post = (req, res, next) => {
       });
     }
     else {
-      FileSystemController.upload_file(req.file, doc._id, function(error){
-        if(error){
-          res.render('error.html', {
-            title: 'System Error',
-            errorCode: 'upload Error',
-            errorMessage: err
-          });
-        }
-        else{
-          res.redirect('/personal/file');
-        }
-      });
+      if (req.file != null) {
+        FileSystemController.upload_file(req.file, doc._id, function (error) {
+          if (error) {
+            res.render('error.html', {
+              title: 'System Error',
+              errorCode: 'upload Error',
+              errorMessage: err
+            });
+          }
+          else {
+            res.redirect('/personal/file');
+          }
+        });
+      }
+      else {
+        res.render('error.html', {
+          title: 'System Error',
+          errorCode: 'System Error',
+          errorMessage: 'Upload file empty.'
+        });
+      }
     }
   })
 }
@@ -74,15 +83,15 @@ const file_delete_post = (req, res, next) => {
       });
     }
     else {
-      FileSystemController.deleteFile(req.body.filename, doc._id, function(error){
-        if(error){
+      FileSystemController.deleteFile(req.body.filename, doc._id, function (error) {
+        if (error) {
           res.render('error.html', {
             title: 'System Error',
             errorCode: 'upload Error',
             errorMessage: err
           });
         }
-        else{
+        else {
           res.redirect('/personal/file');
         }
       });
