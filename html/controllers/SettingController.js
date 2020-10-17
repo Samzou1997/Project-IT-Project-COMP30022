9bccfbb437bc1286c29457d0b4a3210cce3b1bc5
Hang Zou <samzou1997@gmail.com>
2020-10-18 05:18:22 +1100

    debug

--- html/controllers/SettingController.js
+++ html/controllers/SettingController.js
@@ -40,7 +40,7 @@ const setting_get = (req, res, next) => {
       var dataCleanStr = JSON.stringify(date);
       console.log(dataCleanStr);
       //date = moment(date, "MM-DD-YYYY");
-      webPageDate.setting.dateofbirth = date;
+      webPageDate.setting.dateofbirth = dataCleanStr;
       webPageDate.setting.gender = doc.details.gender;
       webPageDate.setting.phone = doc.details.phone;
       webPageDate.setting.address = doc.details.address;
